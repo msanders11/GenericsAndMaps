c4df813798eebbd686f9a64cfb895b5241c1fe09
msanders11 <msanders11@L11713042776.wctc.msft>
2017-04-25 19:59:15 -0500

    Initial git

---
+++ src/common/Movie.java
@@ -0,0 +1,36 @@
+package common;
+
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
+/**
+ * For practice, add the required equals, hashCode and toString methods. Then
+ * add a Comparable interface for the "natural" default sort order. Remember,
+ * consider what makes these objects unique? Also consider that only one
+ * field can be used for sorting. What will you do?
+ * 
+ */
+public class Movie  {
+    private String title;
+    private String director;
+
+    public Movie() {
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    
+
+}
